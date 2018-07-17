df3468b4f4f604bad641fefc751446ee88b228f1
sreekanth-kc <kcsreekanth123@gmail.com>
2018-07-17 18:53:21 +0530

    Group commit

---
+++ 1.php
@@ -0,0 +1,14 @@
+<?php
+echo "I'm about to learn PHP\n";
+echo "You could use zend/symfony\n";
+echo "I'ts called \"Product Tree\" \n";
+echo "\\0 called a null character\n";
+
+/*
+1. Write a PHP script to print the following,
+● 'I'm about to learn PHP!'
+● 'You could use Zend\Symfony'
+● "It's called "Product Tree""
+● "\0 is called as a null character"
+Use the inputs as such (including the quotes)
+*/
